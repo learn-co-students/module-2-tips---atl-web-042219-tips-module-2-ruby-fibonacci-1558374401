a13ba3f9b2cc13a9512ce562791e2ca1d28e07fc
Andrew Allen <asallen67@gmail.com>
2019-05-20 14:07:32 -0400

    Done.

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,20 @@
-require 'pry-byebug'
+# require 'pry-byebug'
+require 'pry'
 # Pry is included in case you'd like to run this file and test it,
 # but you may remove it if you like.
 
 def fibonacci(n)
-  # your code here :)
+	if n < 1
+		"No."
+	elsif n < 3
+		1
+	else
+		fibonacci(n-2)+fibonacci(n-1)
+	end
 end
 
 Pry.start
+
+# for k in 1..20 do
+# 	print fibonacci(k).to_s+', '
+# end
